a2d9ecdab77809930859a9e68c4814e3e4d6f41d
Caesario Prima Bahari <crio.0592@gmail.com>
2016-09-06 14:43:14 +0700

    ganti header

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BeReady</title>
+    <link rel="shortcut icon" href="http://localhost/beready/asset/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="http://localhost/beready/asset/img/favicon.ico" type="image/x-icon">
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="http://localhost/beready/asset/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="http://localhost/beready/asset/css/freelancer.css" rel="stylesheet">
+    <link type="text/css" href="http://localhost/beready/asset/css/fileinput.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://localhost/beready/asset/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://localhost/beready/asset/css/Montserrat.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <script src="http://localhost/beready/asset/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="http://localhost/beready/asset/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://localhost/beready/asset/js/jquery.easing.min.js"></script>
+    <script src="http://localhost/beready/asset/js/classie.js"></script>
+    <script src="http://localhost/beready/asset/js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="http://localhost/beready/asset/js/jqBootstrapValidation.js"></script>
+    <script src="http://localhost/beready/asset/js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="http://localhost/beready/asset/js/freelancer.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+    <!-- Header -->
+    
+
+    <!-- jQuery -->
+    
